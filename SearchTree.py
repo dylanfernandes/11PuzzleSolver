5482e5db1b14955dd67c0c1c6f71f41822b3b8ff
Ahmad El-Baher <ahmad.elbaher@gmail.com>
2018-09-23 21:49:13 -0700

    Created main script and added get path function
    Added get path function for the tree node class.

--- SearchTree.py
+++ SearchTree.py
@@ -28,3 +28,26 @@ class TreeNode:
     def get_right_child(self):
         return self.right_child
 
+    def get_parent(self):
+        return self.parent
+
+    def __str__(self):
+        return self.data.__str__()
+
+    def get_path_to_node(self, tree_node_to_find):
+        return self._get_path_to_node(self, tree_node_to_find)
+
+    # Gets the shortest path to the tree node
+    def _get_path_to_node(self, curr_tree_node, tree_node_to_find):
+        if curr_tree_node is tree_node_to_find:
+            return curr_tree_node.__str__()
+        elif curr_tree_node is None:
+            path_left = curr_tree_node.__str__() + self._get_path_to_node(curr_tree_node.left_child, tree_node_to_find)
+            path_right = curr_tree_node.__str__() + self._get_path_to_node(curr_tree_node.right_child, tree_node_to_find)
+            if len(path_left) > len(path_right):
+                return path_left
+            else:
+                return path_right
+        else:
+            return ""
+
---
+++ main_script.py
@@ -0,0 +1,24 @@
+
+
+def main():
+    """ Main code goes here"""
+
+# Execute main here
+main()
+
+# Other methods
+
+def heuristic_1():
+    return
+
+def heuristic_2():
+    return
+
+def depth_first_search():
+    return
+
+def best_first_search(heuristic_func):
+    return
+
+def a_star(heuristic_func):
+    return
