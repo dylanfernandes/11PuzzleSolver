5feb669bbe0c8768bb91776a3310ee456d57b762
dylanfernandes <dylangfernandes@gmail.com>
2018-09-23 22:47:29 -0400

    Modified init function to take all board elements

--- board.py
+++ board.py
@@ -1,8 +1,19 @@
 class Board:
     elements = []
     SIZE = 12
-    def __init__(self):
-        self.elements = [0]*self.SIZE
+    def __init__(self, e0 = 0, e1 = 0, e2 = 0, e3 = 0, e4 = 0, e5 = 0, e6 = 0, e7 = 0, e8 = 0, e9 = 0, e10 = 0, e11 = 0):
+        self.elements.insert(0 ,e0)
+        self.elements.insert(1 ,e1)
+        self.elements.insert(2 ,e2)
+        self.elements.insert(3 ,e3)
+        self.elements.insert(4 ,e4)
+        self.elements.insert(5 ,e5)
+        self.elements.insert(6 ,e6)
+        self.elements.insert(7 ,e7)
+        self.elements.insert(8 ,e8)
+        self.elements.insert(9 ,e9)
+        self.elements.insert(10 ,e10)
+        self.elements.insert(11 ,e11)
 
     def printBoard(self):
         print self.elements
--- boardTest.py
+++ boardTest.py
@@ -1,4 +1,4 @@
 from board import Board
 
-board = Board()
+board = Board(0,1,2,3,4,5,6,7,8,9,10,11)
 board.printBoard()
