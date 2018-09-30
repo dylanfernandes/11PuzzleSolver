2cc5b05590e74ff7183e078091cfc61219a5ce0b
dylanfernandes <dylangfernandes@gmail.com>
2018-09-30 16:50:46 -0400

    Added tracnking for space + unit tests for board

--- board.py
+++ board.py
@@ -1,6 +1,9 @@
 class Board:
     elements = []
     SIZE = 12
+    ROWSIZE = 4
+    spacePos = 0
+    SPACETOKEN = 0
     def __init__(self, e0 = 0, e1 = 0, e2 = 0, e3 = 0, e4 = 0, e5 = 0, e6 = 0, e7 = 0, e8 = 0, e9 = 0, e10 = 0, e11 = 0):
         self.elements.insert(0 ,e0)
         self.elements.insert(1 ,e1)
@@ -15,23 +18,32 @@ class Board:
         self.elements.insert(10 ,e10)
         self.elements.insert(11 ,e11)
 
+    def findSpace(self):
+        index = 0
+        for i in self.elements:
+            if i == self.SPACETOKEN:
+                break
+            index = index + 1
+        self.spacePos = index
+        return index
+
     def printBoard(self):
         print self.elements
 
+    #index starts at 0
     def getColumn(self, num):
         column = []
         index = num
-        print "index: " + `index`
-        print "Length: " + `len(self.elements)`
-        print "Element: " + `self.elements[4]`
         for i in range(0,3): #3 values per column
             column.insert(i, self.elements[index])
             index = index + 4 #next column starts 4 elements later
         return column
 
+    #index sarts at 0
     def getRow(self, num):
         row = []
+        start = num * self.ROWSIZE
         for i in range(0,4):
-            row.insert(i, self.elements[num+i])
+            row.insert(i, self.elements[start+i])
         return row
 
--- boardTest.py
+++ boardTest.py
@@ -1,6 +1,33 @@
+import unittest
 from board import Board
 
-board = Board(0,1,2,3,4,5,6,7,8,9,10,11)
-board.printBoard()
-print board.getColumn(0)
-print board.getRow(0)
+class TestBoard(unittest.TestCase):
+    board = Board(8,1,2,3,4,5,6,7,0,9,10,11)
+
+    def testBoardInitialization(self):
+        self.assertEqual(len(self.board.elements), 12)
+        self.assertEqual(self.board.elements[0], 8)
+        self.assertEqual(self.board.findSpace(), 8)
+
+    def testGetRow(self):
+        row = self.board.getRow(1)
+        self.assertEqual(len(row), 4)
+        self.assertEqual(row[0], 4)
+        self.assertEqual(row[1], 5)
+        self.assertEqual(row[2], 6)
+        self.assertEqual(row[3], 7)
+
+    def testGetColumn(self):
+        col = self.board.getColumn(1)
+        self.assertEqual(len(col), 3)
+        self.assertEqual(col[0], 1)
+        self.assertEqual(col[1], 5)
+        self.assertEqual(col[2], 9)
+
+
+if __name__ == '__main__':
+        unittest.main()
+#board = Board(8,1,2,3,4,5,6,7,0,9,10,11)
+#board.printBoard()
+#print board.getColumn(0)
+#print board.getRow(0)
