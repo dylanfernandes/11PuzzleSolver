81a1a56265c3f2e97c853f2952026d0558347cb8
Ahmad El-Baher <ahmad.elbaher@gmail.com>
2018-10-05 19:23:27 -0700

    Refactored code and added getting path to solution
    Made a common function for best first and A* searches, and a function
    for getting the solution path.

--- main_script.py
+++ main_script.py
@@ -6,9 +6,11 @@ from board import Board
 # constants
 GOAL_STATE = 0
 COST_PER_MOVE = 1
-ERRONEOUS_HEURISTIC = sys.maxint  # The erroneous heuristic value is the largest possible int to guarantee it not being used
+# The erroneous heuristic value is the largest possible int to guarantee it not being used (REMOVE WHEN NOT NEEDED)
+ERRONEOUS_HEURISTIC = sys.maxint
 
 # Also include file I/O for the execution output
+
 
 def main():
     """ Main code goes here"""
@@ -27,7 +29,10 @@ def main():
 # Other methods
 
 
-def heuristic_1(puzzle, move):
+def heuristic_1(puzzle, move=-1):
+    heuristic_value = 0
+    if move >= 0:
+        puzzle.makeMove(move)
     return ERRONEOUS_HEURISTIC
 
 
@@ -42,6 +47,7 @@ def heuristic_2(puzzle, move=-1):
     :return: the heuristic value, which is the total number of pieces that are not in their correct positions.
     """
     heuristic_value = 0
+    # Give the option of calculating the heuristic value without making a move
     if move >= 0:
         puzzle.makeMove(move)
 
@@ -92,6 +98,14 @@ def depth_first_search():
 
 
 def best_first_search(puzzle, heuristic_func):
+    return search_with_priority(puzzle, heuristic_func, 0)
+
+
+def a_star(puzzle, heuristic_func):
+    return search_with_priority(puzzle, heuristic_func, COST_PER_MOVE)
+
+
+def search_with_priority(puzzle, heuristic_func, cost_per_move):
 
     # Form auxiliary data structures
     closed_list = []
@@ -120,70 +134,21 @@ def best_first_search(puzzle, heuristic_func):
             # get solution path to node
             print("SOLVED!")
             puzzle.printBoard()
+            print("Solution " + str(get_solution_path(tree_node_to_check)))
 
             # clear open list to end loop
             while len(open_list_pq) > 0:
                 heapq.heappop(open_list_pq)
 
-            return  # the solution
-        else:
-
-            # Find add add possible puzzle configs as children to tree_node_to_check here
-            # Also use heuristic_func to set their heuristic values
-            possible_moves = puzzle.determineMoves()
-            for move in possible_moves:
-                heuristic_value = heuristic_func(puzzle, move)
-                tree_node_to_check.create_and_add_child_with_cost(move, heuristic_value, 0)
-
-            for child in tree_node_to_check.get_children:
-                heapq.heappush(open_list_pq, child)
-
-            if len(open_list_pq) == 0:
-                print("Solution not found!")
-                return None
-
-
-def a_star(puzzle, heuristic_func):
-
-    # Form auxiliary data structures
-    closed_list = []
-
-    # Form priority queue
-    open_list = []
-    open_list_pq = heapq.heapify(open_list)
-
-    # Form first tree node and add it to queue to begin
-    search_tree = TreeNode(puzzle)
-    heuristic_value = heuristic_func(search_tree.get_data())
-    search_tree.set_heuristic_value(heuristic_value)
-    heapq.heappush(open_list_pq, search_tree)
-
-    # Game loop
-    while len(open_list_pq) > 0:
-
-        tree_node_to_check = heapq.heappop(open_list_pq)
-        closed_list.append(tree_node_to_check)
-
-        puzzle.makeMove(tree_node_to_check.get_data())
-
-        # Find better way to check goal state
-        if tree_node_to_check.get_heuristic_value() == GOAL_STATE:
-
-            # get solution path to node
-
-            # clear open list to end loop
-            while len(open_list_pq) > 0:
-                heapq.heappop(open_list_pq)
-
-            return  # the solution
+            return None  # the solution
         else:
 
             # Find add add possible puzzle configs as children to tree_node_to_check here
             # Also use heuristic_func to set their heuristic values and set cost as well
             possible_moves = puzzle.determineMoves()
             for move in possible_moves:
-                heuristic_value = heuristic_func(puzzle)
-                tree_node_to_check.create_and_add_child_with_cost(move, heuristic_value, COST_PER_MOVE)
+                heuristic_value = heuristic_func(puzzle, move)
+                tree_node_to_check.create_and_add_child_with_cost(move, heuristic_value, cost_per_move)
 
             for child in tree_node_to_check.get_children:
                 heapq.heappush(open_list_pq, child)
@@ -191,6 +156,18 @@ def a_star(puzzle, heuristic_func):
             if len(open_list_pq) == 0:
                 print("Solution not found!")
                 return None
+
+
+# NOTE: CHANGE TO HAVE STRING REP OF ITEMS
+# go up the parents and put them in array
+def get_solution_path(leaf_node):
+    solution_path = []
+    if len(leaf_node.get_children()) == 0:
+        curr_tree_node = leaf_node
+        while curr_tree_node is not None:
+            solution_path.insert(0, curr_tree_node)
+            curr_tree_node = curr_tree_node.get_parent()
+    return solution_path
 
 
 # Execute main here
