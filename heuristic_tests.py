e0380f84bcfce5a6471ac291458c5ff3eb42955c
dylanfernandes <dylangfernandes@gmail.com>
2018-10-13 15:52:17 -0400

    Added two test cases for heuristic 1

--- heuristic_tests.py
+++ heuristic_tests.py
@@ -26,5 +26,15 @@ class HeuristicTests(unittest.TestCase):
         heuristic_val = heuristic_1(board)
         self.assertEqual(0, heuristic_val)
 
+    def test_h1_invalid_column(self):
+        board = Board([1, 2, 3, 4, 5, 6, 7, 0, 9, 10, 11, 8])
+        heuristic_val = heuristic_1(board)
+        self.assertEqual(217, heuristic_val)
+
+    def test_h1_invalid_rows(self):
+        board = Board([1, 2, 3, 7, 5, 6, 0, 4, 9, 10, 11, 8])
+        heuristic_val = heuristic_1(board)
+        self.assertEqual(334, heuristic_val)
+
 if __name__ == '__main__':
     unittest.main()
