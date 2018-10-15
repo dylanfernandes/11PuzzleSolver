b5000b020ff6251d871adfa3485333f3cbbc3172
Ahmad El-Baher <ahmad.elbaher@gmail.com>
2018-10-15 13:17:58 -0400

    Update experiment
    Fixed global vars

--- experiment/speedExperiment.py
+++ experiment/speedExperiment.py
@@ -10,7 +10,7 @@ row_shuffle = Board([9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8])
 column_shuffle = Board([4, 3, 2, 1, 8, 7, 6, 5, 0, 11, 10 ,9])
 bad_column = Board([1, 2, 3, 8, 5, 6, 7, 0, 9, 10, 11, 4])
 bad_row = Board([1, 2, 3, 4, 5, 6, 7, 8, 11, 0, 10, 9])
-search_functions = [best_first_search, a_star]
+search_functions = [a_star]  # best_first_search, 
 def main():
     compare_heuristics()
     modify_heuristic_1()
@@ -23,7 +23,7 @@ def test(heuristics, boards, board_names):
             boad_name_index = 0
             for board in boards:
                 start = time.time()
-                search_function(board, heuristic)
+                sol = search_function(board, heuristic)
                 end = time.time()
                 print `board_names[boad_name_index]` + "\nTime:  " +  `end - start`
                 print "-------------------------"
@@ -40,20 +40,20 @@ def modify_heuristic_1():
     boards = [mix_1, mix_2, row_shuffle, bad_column]
     board_names = ["mix_1", "mix_2", "row_shuffle","bad_column"]
     test(heuristics, boards, board_names)
-    H1_MODIFIER = [1,2,3,4]
-    print "Modifier applied: " + `H1_MODIFIER`
+    main_script.H1_MODIFIER = [1,2,3,4]
+    print "Modifier applied: " + `main_script.H1_MODIFIER`
     test(heuristics, boards, board_names)
-    H1_MODIFIER = []
+    main_script.H1_MODIFIER = []
 
 def modify_heuristic_2():
         heuristics = [heuristic_2]
         boards = [mix_1, mix_2, row_shuffle, bad_column, bad_row]
         board_names = ["mix_1", "mix_2", "row_shuffle", "bad_column", "bad_row"]
         test(heuristics, boards, board_names)
-        H2_MODIFIER = [25, 100]
-        print "Modifier applied: " + `H2_MODIFIER`
+        main_script.H2_MODIFIER = [0, 1]
+        print "Modifier applied: " + `main_script.H2_MODIFIER`
         test(heuristics, boards, board_names)
-        H2_MODIFIER = []          
+        main_script.H2_MODIFIER = []          
 
 if __name__ == '__main__':
         main()
